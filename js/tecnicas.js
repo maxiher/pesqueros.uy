10ab5cec21c3dc0710fa890378b989bb7a911ce3
MaxiHer <maxihernandez444@gmail.com>
2026-09-01 19:54:45 -0300

    Fotos en Tecnicas

--- js/tecnicas.js
+++ js/tecnicas.js
@@ -14,7 +14,7 @@ fetch("./data/tecnicas.json")
 
             card.innerHTML = `
                 <img
-                    src="${tecnica.imagen}"
+                    src="${tecnica.imagenes}"
                     alt="${tecnica.nombre}"
                     class="card-tecnica-img"
                 >
